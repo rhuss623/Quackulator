7a6ecc53e4c59d391adda7733bef90576535f0c1
Ryan Hussey <rhuss623@gmail.com>
2018-11-01 15:53:23 -0700

    Updates

---
+++ precache-manifest.92578a6fa693433bb06f00951bb74089.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc680983b8b210f7b2af",
+    "url": "/Quackulator/static/js/runtime~main.bc680983.js"
+  },
+  {
+    "revision": "d32da53b0ecc565bca59",
+    "url": "/Quackulator/static/js/main.d32da53b.chunk.js"
+  },
+  {
+    "revision": "528616c236c128ef77de",
+    "url": "/Quackulator/static/js/1.528616c2.chunk.js"
+  },
+  {
+    "revision": "d32da53b0ecc565bca59",
+    "url": "/Quackulator/static/css/main.cd0d636a.chunk.css"
+  },
+  {
+    "revision": "aa1d38f233bc4939c5fdb0c5b6ec6f02",
+    "url": "/Quackulator/index.html"
+  }
+];
